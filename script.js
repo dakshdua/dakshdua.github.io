92f57eda36e4590a61d5ceb067f58c36cf754e3d
Daksh Dua <duadaksh@gmail.com>
2021-01-25 16:21:39 -0500

    small rephrasings

--- script.js
+++ script.js
@@ -4,10 +4,3 @@ addEventListener('click', function (ev) {
         document.querySelector(ev.target.getAttribute('href')).scrollIntoView(true);
     }
 });
-
-// document.body.addEventListener('activate.bs.scrollspy', function() {
-//     var el = document.querySelector('#navbar-list .nav-item.active');
-//     el.previousSibling.style.borderColor = white;
-//     el.nextSibling.style.borderColor = white;
-//     console.log('yuh %s %s', el.previousSibling, el.nextSibling);
-// });
